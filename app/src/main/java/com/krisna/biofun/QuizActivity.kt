3f2b7f25cbc177bad3d4e945c5224b5ab3ef757c
Krisna Sirait <krisnapande00@outlook.com>
2022-07-25 22:23:25 +0700

    added layout on activity_start.xml and all the resources needed

--- app/src/main/java/com/krisna/biofun/QuizActivity.kt
+++ app/src/main/java/com/krisna/biofun/QuizActivity.kt
@@ -3,9 +3,9 @@ package com.krisna.biofun
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
+class QuizActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_quiz)
     }
 }
