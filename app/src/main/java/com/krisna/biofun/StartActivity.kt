99dd2a294b70110430b560f7c57f26e4691d0c97
Krisna Sirait <krisnapande00@outlook.com>
2022-07-25 22:42:13 +0700

    added onClickListener for btnStartQuiz in StartActivity.kt

--- app/src/main/java/com/krisna/biofun/StartActivity.kt
+++ app/src/main/java/com/krisna/biofun/StartActivity.kt
@@ -1,11 +1,24 @@
 package com.krisna.biofun
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.krisna.biofun.databinding.ActivityStartBinding
 
 class StartActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityStartBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_start)
+        binding = ActivityStartBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
+        binding.btnStartquiz.setOnClickListener {
+            val intent = Intent(this, QuizActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
     }
 }
