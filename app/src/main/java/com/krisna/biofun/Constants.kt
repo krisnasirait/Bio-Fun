478a759406cabae57ed7a00d7a43ff53080a768e
Krisna Sirait <krisnapande00@outlook.com>
2022-07-28 20:44:10 +0700

    changes on question list

--- app/src/main/java/com/krisna/biofun/Constants.kt
+++ app/src/main/java/com/krisna/biofun/Constants.kt
@@ -115,24 +115,25 @@ object Constants {
 
         val question9 = Question(
             9,
-            "",
-            "",
-            "",
-            "",
-            "",
-            "",
-            3
+            "Berikut ini merupakan organel pada sel beserta fungsinya yang tepat adalah …",
+            "Ribosom : untuk tempat pernafasan aerob",
+            "Nukleus : sebagai lokasi fotosintesis",
+            "Badan golgi : sintesis dan transport lemak",
+            "Retikulum endoplasma halus : sintesis protein",
+            "Vakuola : tempat mencerna makanan\n",
+            5
         )
         questionList.add(question9)
 
         val question10 = Question(
             10,
-            "",
-            "",
-            "",
-            "",
-            "",
-            "",
+            "Proses perpindahan air dari cairan hipotonis/konsentrasi tinggi melalui membran semi " +
+                    "permeabel dinamakan …",
+            "Difusi",
+            "Difusi difasilitasi",
+            "Osmosis",
+            "Endositosis",
+            "Eksositosis",
             3
         )
         questionList.add(question10)
@@ -151,99 +152,226 @@ object Constants {
 
         val question12 = Question(
             12,
-            "Krenasi terjadi bila sel berada di lingkungan atau larutan …",
-            "Hipotonis",
-            "Hipertonis",
-            "Isotonis",
-            "Bersifat asam",
-            "Bersifat basa",
+            "Dalam sintesis protein terjadi proses transkripsi. Kejadian pada proses transkripsi " +
+                    "adalah …",
+            "Merangkai asam-asam amino di ribosom menjadi protein",
+            "Pencetakan mRNA oleh DNA di dalam inti sel",
+            "Penerjemah kode oleh tRNA yang dibawa oleh mRNA",
+            "Pembentukan tRNA di dalam ribosom oleh mRNABersifat asam",
+            "Keluarnya mRNA dari dalam inti sel menuju ribosom",
             2
         )
         questionList.add(question12)
 
         val question13 = Question(
             13,
-            "Krenasi terjadi bila sel berada di lingkungan atau larutan …",
-            "Hipotonis",
-            "Hipertonis",
-            "Isotonis",
-            "Bersifat asam",
-            "Bersifat basa",
+            ". Di bawah ini adalah pasangan yang menyatakan karakteristik khusus sel dari tumbuhan " +
+                    "adalah …",
+            "Mitokondria dan lisosom",
+            "Kloroplas dan dinding sel",
+            "Retikulum endoplasma dan kompleks golgi",
+            "Vakuola dan nukleus",
+            "Membran plasma dan nukleus",
             2
         )
         questionList.add(question13)
 
         val question14 = Question(
             14,
-            "Krenasi terjadi bila sel berada di lingkungan atau larutan …",
-            "Hipotonis",
-            "Hipertonis",
-            "Isotonis",
-            "Bersifat asam",
-            "Bersifat basa",
+            "Berikut ini yang bukan tergolong dalam organel sel adalah …",
+            "Mitokondria",
+            "Mesosom",
+            "Ribosom",
+            "Nukleus",
+            "Membran plasma",
             2
         )
         questionList.add(question14)
 
         val question16 = Question(
             16,
-            "Krenasi terjadi bila sel berada di lingkungan atau larutan …",
-            "Hipotonis",
-            "Hipertonis",
-            "Isotonis",
-            "Bersifat asam",
-            "Bersifat basa",
-            2
+            "Organel yang berupa saluran halus dalam sitoplasma yang dibatasi sistem membran " +
+                    "dan erat kaitannya dengan sistem angkutan pada sintesis protein yaitu …",
+            "Ribosom",
+            "Retikulum endoplasma",
+            "Plasmodesmata",
+            "Badan golgi",
+            "Lisosom",
+            4
         )
         questionList.add(question16)
 
         val question17 = Question(
             17,
-            "Krenasi terjadi bila sel berada di lingkungan atau larutan …",
-            "Hipotonis",
-            "Hipertonis",
-            "Isotonis",
-            "Bersifat asam",
-            "Bersifat basa",
-            2
+            "Transport aktif merupakan transport yang …",
+            "Mengandalkan perbedaan konsentrasi larutan sebelah dalam dan luar membran sel",
+            "Mengeluarkan energi",
+            "Mengonsumsi energi untuk mengeluarkan atau memasukkan ion-ion atau " +
+                    "molekul melalui membran sel",
+            "Berjalan ke dua arah yang dipengaruhi muatan listrik",
+            "Dipengaruhi oleh ion-ion Na+ dan K+ tanpa energi",
+            3
         )
         questionList.add(question17)
 
         val question18 = Question(
             18,
-            "Krenasi terjadi bila sel berada di lingkungan atau larutan …",
-            "Hipotonis",
-            "Hipertonis",
-            "Isotonis",
-            "Bersifat asam",
-            "Bersifat basa",
-            2
+            "Peristiwa mengkerutnya sel pada sel tumbuhan karena air disebut …",
+            "Plasmolisis",
+            "Hemolisis",
+            "Krenasi",
+            "Hipotonik",
+            "Endositosis",
+            1
         )
         questionList.add(question18)
 
         val question19 = Question(
             19,
-            "Krenasi terjadi bila sel berada di lingkungan atau larutan …",
-            "Hipotonis",
-            "Hipertonis",
-            "Isotonis",
-            "Bersifat asam",
-            "Bersifat basa",
-            2
+            "Membran plasma terdiri atas komponen-komponen berikut kecuali …",
+            "Karbohidrat",
+            "Fosfolipida",
+            "Glikoprotein",
+            "Sterol",
+            "Glikolipida",
+            4
         )
         questionList.add(question19)
 
         val question20 = Question(
             20,
-            "Krenasi terjadi bila sel berada di lingkungan atau larutan …",
-            "Hipotonis",
-            "Hipertonis",
-            "Isotonis",
-            "Bersifat asam",
-            "Bersifat basa",
+            "Proses sintesis ATP dalam sel terjadi di organel …",
+            "Peroksisim dan mitokondria",
+            "Kloroplas dan mitokondria",
+            "Nukleus dan badan golgi",
+            "Kloroplas dan ribosom",
+            "Mitokondria dan lisosom",
             2
         )
         questionList.add(question20)
+
+        val question21 = Question(
+            21,
+            "Badan mikro yang berfungsi menguraikan asam lemak menjadi karbohidrat adalah …\n",
+            "Ribosom",
+            "Lisosom",
+            "Peroksisom",
+            "Kloroplas",
+            "Glikosisom",
+            5
+        )
+        questionList.add(question21)
+
+        val question22 = Question(
+            22,
+            "Membran plasma terdiri dari dua lapis yang tersusun dari senyawa …",
+            "Lipida",
+            "Fosfolipida",
+            "Lipoprotein",
+            "Protein",
+            "Glikolipida",
+            3
+        )
+        questionList.add(question22)
+
+        val question23 = Question(
+            23,
+            "Unsur penting sebagai medium reaksi kimia dan merupakan bahan penyusun " +
+                    "sitoplasma adalah …",
+            "Lemak",
+            "Garam mineral",
+            "Karbohidrat",
+            "Protein",
+            "Air",
+            5
+        )
+        questionList.add(question23)
+
+        val question24 = Question(
+            24,
+            "Proses sintesis ATP dalam sel terjadi di organel …Seorang siswa mencium aroma bau bunga melati yang sedang mekar di halaman " +
+                    "sekolah saat pagi hari adalah salah satu contoh dari proses …",
+            "Transport aktif",
+            "Difusi",
+            "Osmosis",
+            "Pertukaran zat",
+            "Osmosis terfasilitasi",
+            2
+        )
+        questionList.add(question24)
+
+        val question25 = Question(
+            25,
+            "Peristiwa difusi terjadi pada hal-hal berikut kecuali …",
+            "Asap rokok dalam ruangan",
+            "Sirup yang dimasukkan ke dalam air",
+            "Parfum yang disemprotkan ke dalam ruangan",
+            "Potongan umbi kentang dalam air",
+            "Teh celup dalam air panas",
+            4
+        )
+        questionList.add(question25)
+
+        val question26 = Question(
+            26,
+            "Organel yang berupa saluran halus dalam sitoplasma yang dibatasi sistem membran " +
+                    "dan erat kaitannya dengan sistem angkutan pada sintesis protein yaitu …",
+            "Ribosom",
+            "Retikulum endoplasma",
+            "Badan golgi",
+            "Lisosom",
+            "Plasmodemata",
+            3
+        )
+        questionList.add(question26)
+
+        val question27 = Question(
+            27,
+            "Di bawah ini yang tidak termasuk fungsi retikulum endoplasma adalah …",
+            "Detoksifikasi",
+            "Sintesis protein",
+            "Sintesis lemak",
+            "Tempat melekatnya ribosom",
+            "Transportasi materi dalam sel",
+            1
+        )
+        questionList.add(question27)
+
+        val question28 = Question(
+            28,
+            "Penghasil energi merupakan fungsi organel …",
+            "Kompleks golgi",
+            "Kloroplas",
+            "Nukleus",
+            "Mitokondria",
+            "Ribosom",
+            4
+        )
+        questionList.add(question28)
+
+        val question29 = Question(
+            29,
+            "Proses sintesis ATP dalam sel terjadi di organel …",
+            "Peroksisim dan mitokondria",
+            "Kloroplas dan mitokondria",
+            "Nukleus dan badan golgi",
+            "Kloroplas dan ribosom",
+            "Mitokondria dan lisosom",
+            2
+        )
+        questionList.add(question29)
+
+        val question30 = Question(
+            30,
+            "Proses sintesis ATP dalam sel terjadi di organel …",
+            "Peroksisim dan mitokondria",
+            "Kloroplas dan mitokondria",
+            "Nukleus dan badan golgi",
+            "Kloroplas dan ribosom",
+            "Mitokondria dan lisosom",
+            2
+        )
+        questionList.add(question30)
 
         return questionList
     }
