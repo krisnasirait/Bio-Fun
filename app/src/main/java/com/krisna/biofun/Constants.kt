6b761bafe2af5be8c27110b9681c3e7de9c83649
Krisna Sirait <krisnapande00@outlook.com>
2022-07-28 19:31:14 +0700

    added more questions

--- app/src/main/java/com/krisna/biofun/Constants.kt
+++ app/src/main/java/com/krisna/biofun/Constants.kt
@@ -16,234 +16,234 @@ object Constants {
         )
         questionList.add(question1)
 
-//        val question2 = Question(
-//            2,
-//            "Berikut merupakan pemahaman sel sebagai unit fungsional terkecil dari makhluk hidup " +
-//                    "ialah …",
-//            "Sel tersusun atas protein struktural dan fungsional",
-//            "Sel melakukan aktivitas hidup seperti organisme",
-//            "Organel sel tersusun atas protein, lemak dan karbohidrat",
-//            "Sel tersusun atas molekul organik dan non organik",
-//            "Sel merupakan memiliki struktur yang khas",
-//            2,
-//        )
-//        questionList.add(question2)
-//
-//        val question3 = Question(
-//            3,
-//            "Membran sel sangat penting karena alasan di bawah ini, kecuali …",
-//            "Mengendalikan pertukaran zat antara sel",
-//            "Menjadi pembatas antara sitoplasma dan nukleoplasma",
-//            "Bersifat diferensial permeabel",
-//            "Menjadi pembatas antara isi sel dengan lingkungan luarnya",
-//            "Sebagai reseptor",
-//            3
-//        )
-//        questionList.add(question3)
-//
-//        val question4 = Question(
-//            4,
-//            "Mitokondria merupakan organel yang memiliki membran rangkap, kista dan matriks. " +
-//                    "Fungsi matriks adalah …",
-//            "Tempat terjadinya respirasi seluler dan menghasilkan ATP",
-//            "Tempat sintesis protein",
-//            "Sintesis lipid ",
-//            "Metabolisme karbohidrat",
-//            "Oksidasi asam lemak dan katabolisme asetil koenzim",
-//            5
-//        )
-//        questionList.add(question4)
-//
-//        val question5 = Question(
-//            5,
-//            "Organ sel yang berperan mengarahkan kromosom ke kutub pada saat pembelahan sel " +
-//                    "adalah …",
-//            "Nukleolus",
-//            "Nukleus",
-//            "Badan golgi",
-//            "Sentriol",
-//            "Ribosom",
-//            4
-//        )
-//        questionList.add(question5)
-//
-//        val question6 = Question(
-//            6,
-//            "Perhatikan ciri-ciri berikut!\n" +
-//                    "1) Materi inti tidak memiliki membran\n" +
-//                    "2) DNA berada di dalam nukleolus\n" +
-//                    "3) Tidak memiliki organel\n" +
-//                    "4) Materi inti memiliki membran\n" +
-//                    "Dari ciri –ciri tersebut, ciri-ciri sel prokariotik ditunjukkan oleh nomor …",
-//            "1 dan 2",
-//            "2 dan 3",
-//            "3 dan 4",
-//            "2 dan 4",
-//            "1 dan 3",
-//            5
-//        )
-//        questionList.add(question6)
-//
-//        val question7 = Question(
-//            7,
-//            "Difusi merupakan peristiwa …",
-//            "Pergerakan molekul dari ruang hampa ke ruang yang berisi udara",
-//            "Pergerakan molekul melalui selaput permiabel",
-//            "Pergerakan molekul dari daerah kerapatan tinggi ke kerapatan rendah",
-//            "Pergerakan molekul dari daerah kerapatan rendah ke kerapatan tinggi",
-//            "Pergerakan molekul tanpa melalui selaput permiabel\n",
-//            3
-//        )
-//        questionList.add(question7)
-//
-//        val question8 = Question(
-//            8,
-//            "Organel sel memiliki ciri-ciri :\n" +
-//                    "1) Berbentuk oval\n" +
-//                    "2) Mempunyai dua lapis membran \n" +
-//                    "3) Membran dalam berlaku untuk memperluas bidang permukaan untuk menyerap \n" +
-//                    "oksigen \n" +
-//                    "Fungsi organ tersebut adalah …",
-//            "Kloroplas sebagai tempat reaksi terang",
-//            "Retikulum endoplasma sebagai penghubung inti dan splasma",
-//            "Mitokondria sebagai alat pengeluaran sisa metabolisme",
-//            "Kloroplas sebagai pembentuk ATP\n",
-//            "Mikrokandia sebagai tempat pembentuk energi",
-//            5
-//        )
-//        questionList.add(question8)
-//
-//        val question9 = Question(
-//            9,
-//            "",
-//            "",
-//            "",
-//            "",
-//            "",
-//            "",
-//            3
-//        )
-//        questionList.add(question9)
-//
-//        val question10 = Question(
-//            10,
-//            "",
-//            "",
-//            "",
-//            "",
-//            "",
-//            "",
-//            3
-//        )
-//        questionList.add(question10)
-//
-//        val question11 = Question(
-//            11,
-//            "Krenasi terjadi bila sel berada di lingkungan atau larutan …",
-//            "Hipotonis",
-//            "Hipertonis",
-//            "Isotonis",
-//            "Bersifat asam",
-//            "Bersifat basa",
-//            2
-//        )
-//        questionList.add(question11)
-//
-//        val question12 = Question(
-//            12,
-//            "Krenasi terjadi bila sel berada di lingkungan atau larutan …",
-//            "Hipotonis",
-//            "Hipertonis",
-//            "Isotonis",
-//            "Bersifat asam",
-//            "Bersifat basa",
-//            2
-//        )
-//        questionList.add(question12)
-//
-//        val question13 = Question(
-//            13,
-//            "Krenasi terjadi bila sel berada di lingkungan atau larutan …",
-//            "Hipotonis",
-//            "Hipertonis",
-//            "Isotonis",
-//            "Bersifat asam",
-//            "Bersifat basa",
-//            2
-//        )
-//        questionList.add(question13)
-//
-//        val question14 = Question(
-//            14,
-//            "Krenasi terjadi bila sel berada di lingkungan atau larutan …",
-//            "Hipotonis",
-//            "Hipertonis",
-//            "Isotonis",
-//            "Bersifat asam",
-//            "Bersifat basa",
-//            2
-//        )
-//        questionList.add(question14)
-//
-//        val question16 = Question(
-//            16,
-//            "Krenasi terjadi bila sel berada di lingkungan atau larutan …",
-//            "Hipotonis",
-//            "Hipertonis",
-//            "Isotonis",
-//            "Bersifat asam",
-//            "Bersifat basa",
-//            2
-//        )
-//        questionList.add(question16)
-//
-//        val question17 = Question(
-//            17,
-//            "Krenasi terjadi bila sel berada di lingkungan atau larutan …",
-//            "Hipotonis",
-//            "Hipertonis",
-//            "Isotonis",
-//            "Bersifat asam",
-//            "Bersifat basa",
-//            2
-//        )
-//        questionList.add(question17)
-//
-//        val question18 = Question(
-//            18,
-//            "Krenasi terjadi bila sel berada di lingkungan atau larutan …",
-//            "Hipotonis",
-//            "Hipertonis",
-//            "Isotonis",
-//            "Bersifat asam",
-//            "Bersifat basa",
-//            2
-//        )
-//        questionList.add(question18)
-//
-//        val question19 = Question(
-//            19,
-//            "Krenasi terjadi bila sel berada di lingkungan atau larutan …",
-//            "Hipotonis",
-//            "Hipertonis",
-//            "Isotonis",
-//            "Bersifat asam",
-//            "Bersifat basa",
-//            2
-//        )
-//        questionList.add(question19)
-//
-//        val question20 = Question(
-//            20,
-//            "Krenasi terjadi bila sel berada di lingkungan atau larutan …",
-//            "Hipotonis",
-//            "Hipertonis",
-//            "Isotonis",
-//            "Bersifat asam",
-//            "Bersifat basa",
-//            2
-//        )
-//        questionList.add(question20)
+        val question2 = Question(
+            2,
+            "Berikut merupakan pemahaman sel sebagai unit fungsional terkecil dari makhluk hidup " +
+                    "ialah …",
+            "Sel tersusun atas protein struktural dan fungsional",
+            "Sel melakukan aktivitas hidup seperti organisme",
+            "Organel sel tersusun atas protein, lemak dan karbohidrat",
+            "Sel tersusun atas molekul organik dan non organik",
+            "Sel merupakan memiliki struktur yang khas",
+            2,
+        )
+        questionList.add(question2)
+
+        val question3 = Question(
+            3,
+            "Membran sel sangat penting karena alasan di bawah ini, kecuali …",
+            "Mengendalikan pertukaran zat antara sel",
+            "Menjadi pembatas antara sitoplasma dan nukleoplasma",
+            "Bersifat diferensial permeabel",
+            "Menjadi pembatas antara isi sel dengan lingkungan luarnya",
+            "Sebagai reseptor",
+            3
+        )
+        questionList.add(question3)
+
+        val question4 = Question(
+            4,
+            "Mitokondria merupakan organel yang memiliki membran rangkap, kista dan matriks. " +
+                    "Fungsi matriks adalah …",
+            "Tempat terjadinya respirasi seluler dan menghasilkan ATP",
+            "Tempat sintesis protein",
+            "Sintesis lipid ",
+            "Metabolisme karbohidrat",
+            "Oksidasi asam lemak dan katabolisme asetil koenzim",
+            5
+        )
+        questionList.add(question4)
+
+        val question5 = Question(
+            5,
+            "Organ sel yang berperan mengarahkan kromosom ke kutub pada saat pembelahan sel " +
+                    "adalah …",
+            "Nukleolus",
+            "Nukleus",
+            "Badan golgi",
+            "Sentriol",
+            "Ribosom",
+            4
+        )
+        questionList.add(question5)
+
+        val question6 = Question(
+            6,
+            "Perhatikan ciri-ciri berikut!\n" +
+                    "1) Materi inti tidak memiliki membran\n" +
+                    "2) DNA berada di dalam nukleolus\n" +
+                    "3) Tidak memiliki organel\n" +
+                    "4) Materi inti memiliki membran\n" +
+                    "Dari ciri –ciri tersebut, ciri-ciri sel prokariotik ditunjukkan oleh nomor …",
+            "1 dan 2",
+            "2 dan 3",
+            "3 dan 4",
+            "2 dan 4",
+            "1 dan 3",
+            5
+        )
+        questionList.add(question6)
+
+        val question7 = Question(
+            7,
+            "Difusi merupakan peristiwa …",
+            "Pergerakan molekul dari ruang hampa ke ruang yang berisi udara",
+            "Pergerakan molekul melalui selaput permiabel",
+            "Pergerakan molekul dari daerah kerapatan tinggi ke kerapatan rendah",
+            "Pergerakan molekul dari daerah kerapatan rendah ke kerapatan tinggi",
+            "Pergerakan molekul tanpa melalui selaput permiabel\n",
+            3
+        )
+        questionList.add(question7)
+
+        val question8 = Question(
+            8,
+            "Organel sel memiliki ciri-ciri :\n" +
+                    "1) Berbentuk oval\n" +
+                    "2) Mempunyai dua lapis membran \n" +
+                    "3) Membran dalam berlaku untuk memperluas bidang permukaan untuk menyerap \n" +
+                    "oksigen \n" +
+                    "Fungsi organ tersebut adalah …",
+            "Kloroplas sebagai tempat reaksi terang",
+            "Retikulum endoplasma sebagai penghubung inti dan splasma",
+            "Mitokondria sebagai alat pengeluaran sisa metabolisme",
+            "Kloroplas sebagai pembentuk ATP\n",
+            "Mikrokandia sebagai tempat pembentuk energi",
+            5
+        )
+        questionList.add(question8)
+
+        val question9 = Question(
+            9,
+            "",
+            "",
+            "",
+            "",
+            "",
+            "",
+            3
+        )
+        questionList.add(question9)
+
+        val question10 = Question(
+            10,
+            "",
+            "",
+            "",
+            "",
+            "",
+            "",
+            3
+        )
+        questionList.add(question10)
+
+        val question11 = Question(
+            11,
+            "Krenasi terjadi bila sel berada di lingkungan atau larutan …",
+            "Hipotonis",
+            "Hipertonis",
+            "Isotonis",
+            "Bersifat asam",
+            "Bersifat basa",
+            2
+        )
+        questionList.add(question11)
+
+        val question12 = Question(
+            12,
+            "Krenasi terjadi bila sel berada di lingkungan atau larutan …",
+            "Hipotonis",
+            "Hipertonis",
+            "Isotonis",
+            "Bersifat asam",
+            "Bersifat basa",
+            2
+        )
+        questionList.add(question12)
+
+        val question13 = Question(
+            13,
+            "Krenasi terjadi bila sel berada di lingkungan atau larutan …",
+            "Hipotonis",
+            "Hipertonis",
+            "Isotonis",
+            "Bersifat asam",
+            "Bersifat basa",
+            2
+        )
+        questionList.add(question13)
+
+        val question14 = Question(
+            14,
+            "Krenasi terjadi bila sel berada di lingkungan atau larutan …",
+            "Hipotonis",
+            "Hipertonis",
+            "Isotonis",
+            "Bersifat asam",
+            "Bersifat basa",
+            2
+        )
+        questionList.add(question14)
+
+        val question16 = Question(
+            16,
+            "Krenasi terjadi bila sel berada di lingkungan atau larutan …",
+            "Hipotonis",
+            "Hipertonis",
+            "Isotonis",
+            "Bersifat asam",
+            "Bersifat basa",
+            2
+        )
+        questionList.add(question16)
+
+        val question17 = Question(
+            17,
+            "Krenasi terjadi bila sel berada di lingkungan atau larutan …",
+            "Hipotonis",
+            "Hipertonis",
+            "Isotonis",
+            "Bersifat asam",
+            "Bersifat basa",
+            2
+        )
+        questionList.add(question17)
+
+        val question18 = Question(
+            18,
+            "Krenasi terjadi bila sel berada di lingkungan atau larutan …",
+            "Hipotonis",
+            "Hipertonis",
+            "Isotonis",
+            "Bersifat asam",
+            "Bersifat basa",
+            2
+        )
+        questionList.add(question18)
+
+        val question19 = Question(
+            19,
+            "Krenasi terjadi bila sel berada di lingkungan atau larutan …",
+            "Hipotonis",
+            "Hipertonis",
+            "Isotonis",
+            "Bersifat asam",
+            "Bersifat basa",
+            2
+        )
+        questionList.add(question19)
+
+        val question20 = Question(
+            20,
+            "Krenasi terjadi bila sel berada di lingkungan atau larutan …",
+            "Hipotonis",
+            "Hipertonis",
+            "Isotonis",
+            "Bersifat asam",
+            "Bersifat basa",
+            2
+        )
+        questionList.add(question20)
 
         return questionList
     }
